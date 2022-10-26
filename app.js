bfc09eda44475af10680efe65b000b3416079701
Ayub <ayubhaji34@gmail.com>
2022-10-26 12:02:21 -0600

    second commit

--- app.js
+++ app.js
@@ -1,38 +1,32 @@
 // Write your JS in here
 var pics = [
-	"Savannah cat/F1_SavannahCat.jpg",  //0
-	"Savannah cat/Savannah_Cat1.jpg",	    //1
-	"Savannah cat/laying_bored.jpg",    //2
-	"Savannah cat/Savannah.jpg",    //3
-	"Savannah cat/savannah_cat_F1.jpg", //4
-	"Savannah cat/Long_boy.jpg",        //5
-	"Savannah cat/Savannah_Cat_portrait.jpg",//6
-	"Savannah cat/baby_savannah.jpg", //7
-	"Savannah cat/good_boy.jpg" //8
+	"Savannah cat/Savannah_Cat1.jpg",	    //0
+	"Savannah cat/laying_bored.jpg",    //1
+	"Savannah cat/Savannah.jpg",    //2
+	"Savannah cat/savannah_cat_F1.jpg", //3
+	"Savannah cat/Long_boy.jpg",        //4
+	"Savannah cat/Savannah_Cat_portrait.jpg",//5
+	"Savannah cat/baby_savannah.jpg", //6
+	"Savannah cat/good_boy.jpg" //7
 ];
+let length = pics.length;
 // to enable the button to switch among pics going forward
 var btn = document.querySelector("button");
 var img = document.querySelector("img");
-var counter = 1;
+var counter = 0;
 btn.addEventListener("click",function(){
+	counter = counter + 1;
 	if(counter === 8) {
 		counter = 0; 
 	}
-	{
 	img.src = pics[counter];
-	counter = counter + 1;
-	}
 });
 // to enable the botton to switch among pics going backwards
 var btn2 = document.querySelector("button2");
 btn2.addEventListener("click",function(){
-	if(counter === 0){
-		counter = 8;
-	}
-	{
-	img.src = pics[counter];
 	counter = counter - 1;
-	}	
-
-	
+	if(counter < 0){
+		counter = length - 1;
+	}
+	img.src = pics[counter];
 });
